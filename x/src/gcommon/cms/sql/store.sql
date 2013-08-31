4d76b1f19f5ab3c3a2aa9b0f9862064e9348d179
liuwanglei <liuwanglei2001@163.com>
2013-09-01 00:01:27 +0800

    add product sql

--- x/src/gcommon/cms/sql/store.sql
+++ x/src/gcommon/cms/sql/store.sql
@@ -73,3 +73,45 @@ CREATE TABLE `resource` (
   `where` varchar(50) NOT NULL,
   PRIMARY KEY (`resource_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `rank` tinyint(4) NOT NULL,
+  `batch_number` varchar(64) NOT NULL, --pi hao --
+  `quantity` int(11) NOT NULL,
+  `total_price` Decimal(32, 2) NOT NULL, --yuan jia--
+  `shop_price` Decimal(32,2) NOT NULL, --now price--
+  `desc` text NOT NULL, 
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `product_term`
+--
+
+CREATE TABLE `product_term` (
+  `product_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `term_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `data` tinyint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`product_id`,`term_id`),
+  KEY `term_id` (`term_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `product_meta`
+--
+
+CREATE TABLE `product_meta` (
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `meta_product_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) DEFAULT NULL,
+  `meta_value` longtext,
+  PRIMARY KEY (`meta_id`),
+  KEY `product_id` (`meta_product_id`),
+  KEY `meta_key` (`meta_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
