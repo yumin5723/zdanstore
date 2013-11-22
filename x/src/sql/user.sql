9ab29b229a8cd056fb366b6515115b9e80e043b0
wanghx <923371411@qq.com>
2013-11-22 16:13:48 +0800

    update changepw

--- x/src/sql/user.sql
+++ x/src/sql/user.sql
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `username` (`username`)
 ) AUTO_INCREMENT = 1000000000
 ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+ALTER TABLE `user` ADD `birthday` DATE NOT NULL AFTER `password` ;
 
 CREATE TABLE IF NOT EXISTS `user_reset_pass` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,3 +26,17 @@ CREATE TABLE IF NOT EXISTS `user_reset_pass` (
   KEY `code` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `nickname` varchar(50) NOT NULL,
+  `birthday` date NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `email_confirmed` varchar(32) NOT NULL DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=19104 DEFAULT CHARSET=utf8
